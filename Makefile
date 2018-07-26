0e61156817e55b7fd8f3887c863463dc29307f77
Hung Mai <hung7quoc61mai@gmail.com>
2018-07-25 19:51:55 -0700

    test VS code's commit

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 Target	:= run
+Ext		:= .exe
 Source	:= $(Target).cpp
 CFlags	:= -std=c++11 -Wall -Werror 
 Links	:= -lglut -lGL -lGLU
 CC		:= g++
 
-$(Target): $(Source)
+$(Target)$(Ext): $(Source)
 	$(CC) $(CFlags) $^ -o $@ $(Links) 
 
 clean:
--- run.cpp
+++ run.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <GL/glut.h>    // OpenGL library for Linux
 
-using namespace std:
+using namespace std;
 
 int main(int argc, char** argv)
 {
+    // Initialize GLUT libraries withh argc and argv
+    glutInit(argc, argv);
 
     return 0;
 }
