3f518cb26eaef8d98a8cbd0e8da321ad8c5dd227
Hung Mai <hung7quoc61mai@gmail.com>
2018-07-29 19:14:23 -0700

    Test MinGW executable

--- run.cpp
+++ run.cpp
@@ -3,10 +3,16 @@
 
 using namespace std;
 
+#define TELLME 1
+
 int main(int argc, char** argv)
 {
     // Initialize GLUT libraries withh argc and argv
     glutInit(argc, argv);
+
+    #if TELLME
+    cout << "Successfull end of main" << endl;
+    #endif
 
     return 0;
 }
